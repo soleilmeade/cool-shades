56163437ad85341ebb48dc6b845d4d533c5ca8b0
Soleil Meade <soleilmeade@Soleils-MacBook-Pro.local>
2019-03-26 20:24:07 -0400

    js build

---
+++ script.js
@@ -0,0 +1,26 @@
+var redShades={
+  title: 'Red',
+  subtitle: 'Rock-N-Stroll',
+  image: './images/product-red.jpg',
+  price: '$99.00',
+}
+var greenShades={
+  title: 'Green',
+  subtitle: 'Rock-N-Stroll',
+  image: './images/product-green.jpg',
+  price: '$99.00',
+}
+var blueShades={
+  title: 'Blue',
+  subtitle: 'Rock-N-Stroll',
+  image: './images/product-blue.jpg',
+  price: '$99.00',
+}
+var blackShades={
+  title: 'Black',
+  subtitle: 'Rock-N-Stroll',
+  image: './images/product-black.jpg',
+  price: '$99.00',
+}
+
+var shades=[redShades,greenShades,blueShades,blackShades];
