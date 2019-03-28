ec57add7a6d89053a2e07659869dac89689bd359
Soleil Meade <soleilmeade@Soleils-MacBook-Pro.local>
2019-03-28 19:56:18 -0400

    js updates

--- script.js
+++ script.js
@@ -2,25 +2,43 @@ var redShades={
   title: 'Red',
   subtitle: 'Rock-N-Stroll',
   image: './images/product-red.jpg',
-  price: '$99.00',
+  price: 99.00,
 }
 var greenShades={
   title: 'Green',
   subtitle: 'Rock-N-Stroll',
   image: './images/product-green.jpg',
-  price: '$99.00',
+  price: 99.00,
 }
 var blueShades={
   title: 'Blue',
   subtitle: 'Rock-N-Stroll',
   image: './images/product-blue.jpg',
-  price: '$99.00',
+  price: 99.00,
 }
 var blackShades={
   title: 'Black',
   subtitle: 'Rock-N-Stroll',
   image: './images/product-black.jpg',
-  price: '$99.00',
+  price: 99.00,
 }
 
 var shades=[redShades,greenShades,blueShades,blackShades];
+
+function mobileProducts(product) {
+  return `
+    <div class="col-md-3 peppers-product" "text center">
+      <img class="img-fluid" src="${product.image}" alt="Peppers Rock-N-Stroll Red">
+      <p class="product-title">${product.subtitle}</p>
+      <h3>${product.title}</h3>
+      <p class="product-price">$${product.price}</p>
+      <div class="product-cta-container">
+        <a href="#" class="btn btn-danger btn-lg">Add to Cart</a>
+      </div>
+    </div>
+  `;
+}
+
+$(document).ready(function(){
+  $('#mobile-products').html(mobileProducts(greenShades));
+});
